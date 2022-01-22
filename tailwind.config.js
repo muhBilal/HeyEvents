f6b0e4fde7e28709bd1295b9541e4ec8a0205ac5
aryaaak <aryarizky2303@gmail.com>
2022-01-22 12:17:03 +0700

    Update Setup TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
-  content: [],
+  content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#0C083C',
+        'prime': '#312B78',
+        'grey': '#746E6E',
+        'green': '#1BB13C',
+        'overlay': '#F1F0FF',
+        'background': '#E5E5E5',
+      },
+    },
+    fontWeight: {
+      hairline: 100,
+      'extra-light': 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      'extra-bold': 800,
+      black: 900,
+    }
   },
   plugins: [],
 }
